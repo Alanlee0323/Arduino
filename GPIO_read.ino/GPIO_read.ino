94b0c7d02b5be18d773656ca3206e0d06d4446fd
Alanlee0323 <95911604+Alanlee0323@users.noreply.github.com>
2022-03-12 22:19:45 +0800

    Update GPIO_read.ino

--- GPIO_read.ino/GPIO_read.ino
+++ GPIO_read.ino/GPIO_read.ino
@@ -1,12 +1,15 @@
 #define inputPin 23
 
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
   pinMode(inputPin, INPUT);
 }
 
-void loop() {
-  IOstate = digitalread(inputPin);
+void loop() 
+{
+  int IOstate;
+  IOstate = digitalRead(inputPin);
   Serial.println(IOstate);
   delay(200); // delay 200ms for next GPIO read
 }
